a82fd31732a497898e8bfaf914fe2a694ca1b403
MihiraAthukorala <mahiathukorala2022@gmail.com>
2023-06-06 10:44:46 +0530

    Multiplication Of Two Numbers With Keyboard Input

---
+++ Multiplication.java
@@ -0,0 +1,22 @@
+// Get Multiplication Ansewer Of Two Input Numbers
+
+import java.util.*;
+
+class Multiplication {
+    public static void main(String args[]) {
+        System.out.println("Get Multiplication Answer Of Two Keyboard Input Numbers \n");
+        
+        Scanner input=new Scanner(System.in);
+        int x,y,z;
+
+        System.out.print("Input Your 1st Number : ");
+        x=input.nextInt();
+
+        System.out.print("Input Your 2nd Number : ");
+        y=input.nextInt();
+
+        z=x*y;
+
+        System.out.print("Ypur Answer Is : " +z);
+    }
+}
