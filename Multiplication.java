2cf303313a9c6b6f4f676eda99d8ab7ff3295789
MihiraAthukorala <mahiathukorala2022@gmail.com>
2023-06-11 22:08:22 +0530

    Multiplication 11.6.2023

--- Multiplication.java
+++ Multiplication.java
@@ -1,12 +1,39 @@
 // Get Multiplication Ansewer Of Two Input Numbers
 
+// import java.util.*;
+
+// class Multiplication {
+//     public static void main(String args[]) {
+//         System.out.println("Get Multiplication Answer Of Two Keyboard Input Numbers \n");
+        
+//         Scanner input=new Scanner(System.in);
+//         int x,y,z;
+
+//         System.out.print("Input Your 1st Number : ");
+//         x=input.nextInt();
+
+//         System.out.print("Input Your 2nd Number : ");
+//         y=input.nextInt();
+
+//         z=x*y;
+
+//         System.out.print("Ypur Answer Is : " +z);
+//     }
+// }
+
+
+// Get multiplication Answer of two input numbers
+
 import java.util.*;
 
 class Multiplication {
+
     public static void main(String args[]) {
+
         System.out.println("Get Multiplication Answer Of Two Keyboard Input Numbers \n");
-        
-        Scanner input=new Scanner(System.in);
+
+        Scanner input = new Scanner(System.in);
+
         int x,y,z;
 
         System.out.print("Input Your 1st Number : ");
@@ -16,7 +43,7 @@ class Multiplication {
         y=input.nextInt();
 
         z=x*y;
+        System.out.print("Your Answer Is : " +z);
 
-        System.out.print("Ypur Answer Is : " +z);
     }
 }
